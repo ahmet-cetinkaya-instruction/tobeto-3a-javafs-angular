ee315e951456ebf41364ea42faaa45f0b393f60e
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2024-03-25 16:22:44 +0300

    add ngFor example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,43 @@ export class AppComponent {
   title = 'Java FS 3A Rent A Car';
   counter: number = 0;
 
-  incrementCounter() {
+  brands = [
+    { id: 1, name: 'Audi', discontinued: false },
+    { id: 2, name: 'BMW', discontinued: false },
+    { id: 3, name: 'Chevrolet', discontinued: true },
+    { id: 4, name: 'Ford', discontinued: false },
+    { id: 5, name: 'Honda', discontinued: false },
+    { id: 6, name: 'Hyundai', discontinued: false },
+    { id: 7, name: 'Mercedes-Benz', discontinued: false },
+    { id: 8, name: 'Nissan', discontinued: false },
+    { id: 9, name: 'Toyota', discontinued: false },
+    { id: 10, name: 'Volkswagen', discontinued: false },
+    // // Verilerin fronend'de hali hazırda geldiğini varsayıyoruz.
+    // { id: 11, name: 'Mazda', discontinued: false },
+    // { id: 12, name: 'Kia', discontinued: false },
+    // { id: 13, name: 'Subaru', discontinued: false },
+    // { id: 14, name: 'Volvo', discontinued: false },
+    // { id: 15, name: 'Porsche', discontinued: false },
+    // { id: 16, name: 'Mitsubishi', discontinued: false }
+  ];
+
+  onIncrementCounter() {
     ++this.counter;
+  }
+
+  // pageSize = 10;
+  onLoadMoreBrands() {
+    // // Verilerin fronend'de hali hazırda geldiğini varsayıyoruz.
+    // this.pageSize += 10;
+
+    // Verilerin backend'den geldiğini varsayıyoruz.
+    this.brands.push(
+      { id: 11, name: 'Mazda', discontinued: false },
+      { id: 12, name: 'Kia', discontinued: false },
+      { id: 13, name: 'Subaru', discontinued: false },
+      { id: 14, name: 'Volvo', discontinued: false },
+      { id: 15, name: 'Porsche', discontinued: false },
+      { id: 16, name: 'Mitsubishi', discontinued: false }
+    );
   }
 }
