c7d4adfa7868484d841b87329de1d57e073792f1
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2024-03-25 14:10:06 +0300

    add example components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer.component';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // <app-root></app-root>
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'JavaFS3ARentACar';
+  title = 'Java FS 3A Rent A Car';
+  counter: number = 0;
+
+  incrementCounter() {
+    ++this.counter;
+  }
 }
