f402d255e7a486f7a4896143123b8187f65ac479
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2024-04-01 16:02:24 +0300

    add api service

--- src/app/features/brands/components/brands-list-menu/brands-list-menu.component.ts
+++ src/app/features/brands/components/brands-list-menu/brands-list-menu.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
   EventEmitter,
   Input,
@@ -20,10 +21,10 @@ import { BrandsService } from '../../services/brands.service';
   imports: [CommonModule, MenuComponent],
   templateUrl: './brands-list-menu.component.html',
   styleUrl: './brands-list-menu.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush, // OnPush ile değişikliklerin kontrolü optimize edilir.
 })
 export class BrandsListMenuComponent implements OnInit {
-  @Input() initialSelectedBrandId: number | null = null;
+  @Input() initialSelectedBrandId: number | null = null; // 1. OnPush, yönteminde input değerlerinde değişiklik olduğunda değişikliği algılar.
   @Output() selectBrand = new EventEmitter<BrandListItemDto | null>();
 
   brands!: BrandListItemDto[];
@@ -31,34 +32,43 @@ export class BrandsListMenuComponent implements OnInit {
   initialSelectedBrandIndex: number | null = null;
 
   // brandsService: BrandsService;
-  constructor(private brandsService: BrandsService) {
+  constructor(private brandsService: BrandsService, private change: ChangeDetectorRef) {
     // this.brandsService = brandsService;
   }
 
   // ngOnInit component ilk yerleştiğinde bir kez çalışır.
+  // 2. OnPush, lifecycle hookları tetiklendiğinde değişikliği algılar.
   ngOnInit(): void {
     this.getBrandsList();
   }
 
   getBrandsList() {
-    this.brands = this.brandsService.getBrands();
-    if (this.initialSelectedBrandId) {
-      this.selectedBrand =
-        this.brands.find((brand) => brand.id === this.initialSelectedBrandId) ??
-        null;
-      this.initialSelectedBrandIndex = this.brands.findIndex(
-        (brand) => brand.id === this.initialSelectedBrandId
-      );
-    }
+    this.brandsService.getBrands().subscribe((response) => {
+      this.brands = response;
+
+      if (this.initialSelectedBrandId) {
+        this.selectedBrand =
+          this.brands.find(
+            (brand) => brand.id === this.initialSelectedBrandId
+          ) ?? null;
+        this.initialSelectedBrandIndex = this.brands.findIndex(
+          (brand) => brand.id === this.initialSelectedBrandId
+        );
+      }
+
+      // 3. OnPush, ChangeDetectorRef.markForCheck metodu ile componentin değişiklikleri algılaması sağlanır.
+      this.change.markForCheck();
+    });
   }
 
+  // 2. OnPush, kullancı html üzerinden bir event tetiklendiğinde değişikliği algılar.
   onSelectBrand(brand: BrandListItemDto) {
     this.selectedBrand = this.selectedBrand?.id !== brand.id ? brand : null;
     this.selectBrand.emit(this.selectedBrand);
   }
 
   get brandsMenuItems(): MenuItem[] {
-    return this.brands.map((brand) => {
+    return this.brands?.map((brand) => {
       return {
         label: brand.name,
         click: (_: MouseEvent) => this.onSelectBrand(brand),
--- src/app/features/brands/services/brands.service.ts
+++ src/app/features/brands/services/brands.service.ts
@@ -1,25 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BrandListItemDto } from '../models/brand-list-item-dto';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 }) // Singleton
 export class BrandsService {
-  data = [
-    { id: 1, name: 'Toyota' },
-    { id: 2, name: 'Ford' },
-    { id: 3, name: 'Chevrolet' },
-    { id: 4, name: 'Nissan' },
-    { id: 5, name: 'Honda' },
-    { id: 6, name: 'Jeep' },
-    { id: 7, name: 'Hyundai' },
-    { id: 8, name: 'Dodge' },
-    { id: 9, name: 'Kia' },
-    { id: 10, name: 'GMC' },
-  ];
+  private readonly controllerUrl = `${environment.apiUrl}/brands`;
+  // data = [
+  //   { id: 1, name: 'Toyota' },
+  //   { id: 2, name: 'Ford' },
+  //   { id: 3, name: 'Chevrolet' },
+  //   { id: 4, name: 'Nissan' },
+  //   { id: 5, name: 'Honda' },
+  //   { id: 6, name: 'Jeep' },
+  //   { id: 7, name: 'Hyundai' },
+  //   { id: 8, name: 'Dodge' },
+  //   { id: 9, name: 'Kia' },
+  //   { id: 10, name: 'GMC' },
+  // ];
 
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
 
-  getBrands() {
-    return this.data;
+  getBrands(): Observable<BrandListItemDto[]> {
+    return this.httpClient.get<BrandListItemDto[]>(this.controllerUrl);
   }
 }
